caf780aab53f89fe134f9e47e9eee37bd5bfb6a2
Benjamin J. Kruger <bekroogle@gmail.com>
2015-12-31 09:09:41 -0600

    Made [sticky] nav-bar.

--- js/app.js
+++ js/app.js
@@ -183,6 +183,14 @@ $.event.special.tap.emitTapOnTaphold = false;
       toggleHelpHeading();
     });
 
+    $('#dont-follow').click( function(e) {
+      if ($('#dont-follow:checked').length > 0) {
+        $('.controls').css('position', 'absolute');
+      } else {
+        $('.controls').css('position', 'fixed');
+      }
+    });
+
     $('.cell').each( function(index) {
       $(this).on('taphold', function(evt) {
           killClick = true;
